a8eb0a4207b9fb71b664f9651a8e322da573da1a
Devanshi <163548761+devanshi014@users.noreply.github.com>
2026-06-08 17:57:33 +0530

    Add files via upload

---
+++ Backend/src/utils/sendEmail.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false, // true only for 465, false for 587
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_FROM,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Email sending error:", error);
+    throw new Error("Email could not be sent");
+  }
+};
+
+export default sendEmail;
